f4f8999f12a9880464a04b0da55eba2a086f4468
Andy <wanghaoxin5277@gmail.com>
2021-05-17 20:51:19 +0800

    init

---
+++ src/test/java/com/naturelaw/xiayu/XiayuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturelaw.xiayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
