e96650e65b917b22c2d5fd5711b62cc21ed5d3a9
Andy <wanghaoxin5277@gmail.com>
2021-07-22 10:50:20 +0800

    hotfix-001

--- sys-utils/src/main/java/com/naturelaw/sysutils/SysUtilApplication.java
+++ sys-utils/src/main/java/com/naturelaw/sysutils/SysUtilApplication.java
@@ -1,11 +1,15 @@
 package com.naturelaw.sysutils;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * description:
+ * description: 此类不做任何启动使用,如果不加此类build会报错:
  *
  * @author Administrator
  * @date 2021/7/22 10:43
  */
+@SpringBootApplication
 public class SysUtilApplication {
 	public static void main(String[] args) {
 		System.out.println("ddd");
