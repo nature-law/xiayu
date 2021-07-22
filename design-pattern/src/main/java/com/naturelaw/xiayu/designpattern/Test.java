a13711d467a230bb2c3a3cbfae8f689f5326d2fb
Andy <wanghaoxin5277@gmail.com>
2021-07-22 10:55:08 +0800

    dd

--- design-pattern/src/main/java/com/naturelaw/xiayu/designpattern/Test.java
+++ design-pattern/src/main/java/com/naturelaw/xiayu/designpattern/Test.java
@@ -1,11 +1,14 @@
 package com.naturelaw.xiayu.designpattern;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * description:
  *
  * @author Administrator
  * @date 2021/6/11 10:42
  */
+@SpringBootApplication
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(5 / 4);
